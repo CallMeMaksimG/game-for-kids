057dbe6e8cc7fead0137ffc2edd3fdb050af457e
Maksim Golovanov <maxgolovanov10@gmail.com>
2023-08-26 11:57:27 +0300

    Добавить генерацию рандомных чисел и операторов

---
+++ js/index.js
@@ -0,0 +1,36 @@
+const number1 = document.querySelector('.number1');
+const number2 = document.querySelector('.number2');
+const operator = document.querySelector('.operator1');
+const answer = document.querySelector('.answer');
+
+let correctAnswer = 0;
+
+randomNumbers();
+randomOperator();
+result(number1, number2, operator);
+
+
+function randomNumbers() {
+    number1.innerText = Math.floor(Math.random() * (11 - 1)) + 1;
+    number2.innerText = Math.floor(Math.random() * (11 - 1)) + 1;
+}
+
+function randomOperator() {
+    const operators = ['+', '-'];
+    if (Number(number1.innerText) <= Number(number2.innerText)) {
+        operator.innerText = '+' ;
+    } else if (Number(number1.innerText) >= Number(number2.innerText)){
+        operator.innerText = operators[Math.floor(Math.random() * (2 - 0)) + 0];
+    }
+}
+
+function result(num1, num2, operator) {
+    if (operator.innerText == '+') {
+        
+        correctAnswer = Number(num1.innerText) + Number(num2.innerText);
+        answer.value = correctAnswer;
+    }else if (operator.innerText == '-') {
+        correctAnswer = Number(num1.innerText) - Number(num2.innerText);
+        answer.value = correctAnswer;
+    }
+}
