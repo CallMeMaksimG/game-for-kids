3ceb05bb4903028abc3fde0e989ba24791736ec0
Maksim Golovanov <maxgolovanov10@gmail.com>
2023-08-26 12:28:26 +0300

    Добавить сохранение ответа игрока и сверка его с правильным ответом

--- js/index.js
+++ js/index.js
@@ -2,13 +2,14 @@ const number1 = document.querySelector('.number1');
 const number2 = document.querySelector('.number2');
 const operator = document.querySelector('.operator1');
 const answer = document.querySelector('.answer');
+const answerBtn = document.querySelector('.submit-answer');
 
 let correctAnswer = 0;
+let playerResponse;
 
 randomNumbers();
 randomOperator();
 result(number1, number2, operator);
-
 
 function randomNumbers() {
     number1.innerText = Math.floor(Math.random() * (11 - 1)) + 1;
@@ -28,9 +29,22 @@ function result(num1, num2, operator) {
     if (operator.innerText == '+') {
         
         correctAnswer = Number(num1.innerText) + Number(num2.innerText);
-        answer.value = correctAnswer;
+        // answer.value = correctAnswer;
     }else if (operator.innerText == '-') {
         correctAnswer = Number(num1.innerText) - Number(num2.innerText);
-        answer.value = correctAnswer;
+        // answer.value = correctAnswer;
+    }
+}
+
+answerBtn.addEventListener('click', function saveResponsePlayer() {
+    playerResponse = answer.value;
+    checkResponse();
+});
+
+function checkResponse() {
+    if (Number(playerResponse) == correctAnswer) {
+        console.log("Верно!")
+    } else {
+        console.log("Неверно:(")
     }
 }
